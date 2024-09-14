3735e463f2498ccb1a07d6b998f8961d5522d6fb
Alexey Degtyarik <muhmundr@gmail.com>
2024-09-14 17:25:19 +0300

    Fix bug with injecting css

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+Changelog
+=========
+
+0.1.0
+---
+* Fix bug with injecting css to the bundle
+
+0.0.0
+---
+* Initial Release
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
   plugins: [
@@ -11,6 +12,7 @@ export default defineConfig({
       exclude: ['src/**/*.test.ts', 'src/**/*.test.tsx', 'src/__mocks__/**/*'],
       insertTypesEntry: true,
     }),
+    cssInjectedByJsPlugin(),
   ],
   server: {
     watch: {
